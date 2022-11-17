74f1b8d78ddcb02f0b7b16b01c7a7d96574b32d0
Deimos Karkee <deimoskarrkey@outlook.com>
2022-11-17 19:15:04 +1100

    Close button created and added for feedback items

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import Card from "../UI/Card";
 
 function FeedbackItem({ item }) {
   return (
-    <div className="card">
+    <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close">
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
 }
 
