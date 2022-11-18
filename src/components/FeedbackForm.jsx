85f5deedc7c7856a790e877dc73f6701c9ff4e15
Deimos Karkee <deimoskarrkey@outlook.com>
2022-11-18 11:51:20 +1100

    Rating select comp implemented also fixed current state of text input validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,31 +2,34 @@ import React from "react";
 import { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
   const handelTextChange = (e) => {
-    if (text === "") {
+    const value = e.target.value;
+    if (value === "") {
       setBtnDisabled(true);
       setMessage(null);
-    } else if (text !== "" && text.trim().length <= 10) {
+    } else if (value !== "" && value.trim().length <= 10) {
       setMessage("Text must be at least 10 characters long.");
       setBtnDisabled(true);
     } else {
       setMessage(null);
       setBtnDisabled(false);
     }
-    setText(e.target.value);
+    setText(value);
   };
 
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select compoent */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handelTextChange}
